ce8a5d95a5ef837ea20e76a14a29e583520333a9
knia10 <kelly.stephania@hotmail.com>
2021-11-02 18:44:46 -0500

    update

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # Exactly matching a string that starts with h ends with n and can have any single character
 
-puts ARGV[0].scan(/^[0-9]{3}[0-9]{3}[0-9]){4}$/).join
+puts ARGV[0].scan(/^[0-9]{3}[0-9]{3}[0-9]{3}$/).join
