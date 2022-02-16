783c050b620959e55592ec8f4029c93ce7357baa
knia10 <kelly.stephania@hotmail.com>
2022-02-15 19:37:46 -0500

    Add file 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+'''
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''
+    function that queries the Reddit API and returns the number of subscribers
+    '''
+    url = ("https://www.reddit.com/r/{}/about.json".format(subreddit))
+    user_agent = {'User-Agent': 'My User Agent Custom'}
+    response = requests.get(url, headers=user_agent).json()
+    if response.get('error') == 400:
+        return 0
+    return response.get('data').get('subscribers')
