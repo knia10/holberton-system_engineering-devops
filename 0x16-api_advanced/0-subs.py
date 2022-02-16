ea0f396dadce4e08a8f10e91d3b0d2ca3d2d07eb
knia10 <kelly.stephania@hotmail.com>
2022-02-15 20:14:01 -0500

    Add variable

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,8 +3,6 @@
 function that queries the Reddit API and returns the number of subscribers
 (not active users, total subscribers) for a given subreddit.
 '''
-
-import json
 import requests
 
 
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,9 +3,7 @@
 function that queries the Reddit API and prints the titles of the first
 10 hot posts listed for a given subreddit.
 '''
-
 import requests
-import json
 
 
 def top_ten(subreddit):
@@ -16,8 +14,9 @@ def top_ten(subreddit):
     url = ("https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit))
     user_agent = {'User-Agent': 'My User Agent Custom'}
     response = requests.get(url, headers=user_agent).json()
-    if response.get('error') == 400:
+    get_children = response.get('data').get('children')
+    if response.get('error') == 404:
         print ('None')
     else:
-        for post in response.get('data').get('children'):
+        for post in get_children:
             print(post.get('data').get('title'))
