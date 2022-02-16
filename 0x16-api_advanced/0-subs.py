f010ddd84f28b1c111dea6205832ebf83566733d
knia10 <kelly.stephania@hotmail.com>
2022-02-15 21:29:19 -0500

    1-top_ten.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,6 +3,7 @@
 function that queries the Reddit API and returns the number of subscribers
 (not active users, total subscribers) for a given subreddit.
 '''
+
 import requests
 
 
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,6 +4,7 @@ function that queries the Reddit API and prints the titles of the first
 10 hot posts listed for a given subreddit.
 '''
 import requests
+import json
 
 
 def top_ten(subreddit):
@@ -15,8 +16,8 @@ def top_ten(subreddit):
     user_agent = {'User-Agent': 'My User Agent Custom'}
     response = requests.get(url, headers=user_agent).json()
     get_children = response.get('data').get('children')
-    if get_children:
+    if response.get('error') == 404:
+        print ("None")
+    else:
         for post in get_children:
             print(post.get('data').get('title'))
-    else:
-        print ('None')
