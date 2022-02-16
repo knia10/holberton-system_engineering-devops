be735a0c290310bf488bc255690574ff5b1a2019
knia10 <kelly.stephania@hotmail.com>
2022-02-16 09:06:11 -0500

    Modified oder lines

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -20,12 +20,12 @@ def recurse(subreddit, hot_list=[], after="None"):
     user_agent = {'User-Agent': 'My User Agent Custom'}
     response = requests.get(url, headers=user_agent).json()
     get_children = response.get('data').get('children')
-    get_after = response.get('data').get('after')
-    if response.get('error') == 400:
+    get_after = response.get('data', {}).get('after')
+    if response.get('error') == 404:
         return None
     else:
         for post in get_children:
             hot_list.append(post.get('data').get('title'))
-        if get_after:
-            return recurse(subreddit, hot_list, after)
-        return hot_list
+        if get_after is None:
+            return hot_list
+        return recurse(subreddit, hot_list, after)
