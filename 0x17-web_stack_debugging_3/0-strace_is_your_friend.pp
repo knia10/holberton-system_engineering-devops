83b5240dc309308a11ed86787dd2949569eb2b50
knia10 <kelly.stephania@hotmail.com>
2022-02-16 15:00:39 -0500

    Add directory and files~

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,10 @@
+# fix HTTP/1.0 500 Internal Server Error
+
+exec {'Modified phpp in settings file':
+     command  => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
+     provider => shell
+}
+exec {'restart apache2':
+     command => 'sudo /etc/init.d/apache2 restart',
+     path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
