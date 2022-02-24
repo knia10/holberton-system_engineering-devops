3e670aa7624a9bb43ffe0428f3b4aacbaa39ba4e
knia10 <kelly.stephania@hotmail.com>
2022-02-24 11:29:55 -0500

    Modified file 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,7 @@
-exec {'fix_our_stack_so_that_we_get_to_0':
-  command => "sed -i 's/worker_processes 4/worker_processes auto/g' /etc/nginx/nginx.conf",
+exec {'fix_unlimit':
+  command => "sed -i 's/15/65536/g' /etc/default/nginx",
   path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
 
 exec {'nginx_rstart':
   command => 'nginx restart',
